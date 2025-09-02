ae6be1d96b5fa14354ca567a47c2642d165e2b58
shreyansh <shreyansh.jwd@gmail.com>
2025-09-02 16:50:42 +0530

    Create Common for Input validation and add middleware

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,15 +1,36 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/common-backend/config"
+import { middleware } from "./middleware";
+import { createUserSchema, signinSchema , createRoomSchema} from "@repo/common/test"
 const app = express();
 
 app.post("/signup", (req: Request, res: Response) => {
+
+    const data = createUserSchema.safeParse(req.body);
+
+    if(!data.success){
+        return res.json({
+            message: "Invalid Credentail!!",
+            error: data.error
+        })
+    }
+
     return res.json({
         message: "Signup"
     })
 })
 
 app.post("/signin", (req: Request, res: Response) => {
+    const data = signinSchema.safeParse(req.body);
+
+    if(!data.success){
+        return res.json({
+            message: "Invalid Credentail!!",
+            error: data.error
+        })
+    }
+
     const _id = "123";
 
     const token = jwt.sign({
@@ -21,8 +42,17 @@ app.post("/signin", (req: Request, res: Response) => {
     })
 })
 
-app.post("/create-room", (req: Request, res: Response) => {
+app.post("/create-room", middleware, (req: Request, res: Response) => {
     
+    const data = createRoomSchema.safeParse(req.body);
+
+    if(!data.success){
+        return res.json({
+            message: "Invalid Credentail!!",
+            error: data.error
+        })
+    }
+
     return res.json({
         message: "Signin"
     })
---
+++ apps/http-backend/src/middleware.ts
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/common-backend/config"
+import {Request, Response, NextFunction } from "express"
+
+export const middleware = (req:Request, res: Response, next: NextFunction) => {
+    const { token } = req.headers;
+
+    const decode = jwt.verify(token as string, JWT_SECRET);
+
+    if(!decode){
+        return res.json({
+            message: "Invalid Credentails!!"
+        })
+    }
+    
+    // @ts-ignore
+    req.userId = decode.id;
+    
+    return res.json({
+        message: "Authorized"
+    })
+    next();
+    
+}
