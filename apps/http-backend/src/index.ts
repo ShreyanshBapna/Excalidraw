0f0b8281eb80fd664afd2c699c157afbeaa98da0
shreyansh <shreyansh.jwd@gmail.com>
2025-09-10 16:14:50 +0530

    Add db folder in packages so both the backend can use the same db

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,17 @@
 import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt";
+
 import { JWT_SECRET } from "@repo/common-backend/config"
 import { middleware } from "./middleware";
 import { createUserSchema, signinSchema , createRoomSchema} from "@repo/common/test"
-const app = express();
+import { prismaClient } from "@repo/db/client"
 
-app.post("/signup", (req: Request, res: Response) => {
+
+const app = express();
+app.use(express.json());
+
+app.post("/signup", async (req: Request, res: Response) => {
 
     const data = createUserSchema.safeParse(req.body);
 
@@ -16,12 +22,32 @@ app.post("/signup", (req: Request, res: Response) => {
         })
     }
 
-    return res.json({
-        message: "Signup"
-    })
+    const {email, name, password} = req.body;
+    const hashpassword = await bcrypt.hash(password, 10);
+
+    try {
+        const response = await prismaClient.user.create({
+            data: {
+                email, 
+                name, 
+                password: hashpassword
+            }
+        });
+
+        return res.json({
+            response,
+            message: "Signup successfully!!"
+        })
+    }
+    catch(e) {
+        return res.json({
+            message: "Something went wrong!!",
+            error: e
+        })
+    }
 })
 
-app.post("/signin", (req: Request, res: Response) => {
+app.post("/signin", async (req: Request, res: Response) => {
     const data = signinSchema.safeParse(req.body);
 
     if(!data.success){
@@ -31,15 +57,45 @@ app.post("/signin", (req: Request, res: Response) => {
         })
     }
 
-    const _id = "123";
+    const {email, password} = req.body;
 
-    const token = jwt.sign({
-        id: _id
-    }, JWT_SECRET!);
+    try {
+        // check the user is registerd or not with this email
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email
+            }
+        });
 
-    return res.json({
-        token
-    })
+        if(!user){
+            return res.json({
+                message: "User not found!!"
+            })
+        }
+
+        // check the password is correct or not
+        const checkpassword = await bcrypt.compare(password, user.password);
+
+        if(!checkpassword){
+            return res.json({
+                message: "Invalid Credentail!!"
+            })
+        }
+
+        const token = jwt.sign({
+            id: user.id
+        }, JWT_SECRET!);
+
+        return res.json({
+            token
+        })
+
+    } catch(e) {
+        return res.json({
+            message: "Something went wrong!!",
+            error: e
+        })
+    }
 })
 
 app.post("/create-room", middleware, (req: Request, res: Response) => {
--- packages/common/src/test.ts
+++ packages/common/src/test.ts
@@ -1,9 +1,9 @@
 import  z  from "zod"
 
 export const createUserSchema = z.object({
-    username: z.string().min(5).max(35),
+    name: z.string().min(4).max(35),
     email: z.string().email(),
-    password: z.string().min(8).max(20)
+    password: z.string().min(5).max(20)
 })
 
 export const signinSchema = z.object({
